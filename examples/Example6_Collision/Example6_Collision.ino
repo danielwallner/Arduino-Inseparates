e48f806d026b70b5cc5e90ca804edbfe4ad5b2e3
Daniel Wallner <14888585+danielwallner@users.noreply.github.com>
2024-02-11 22:55:59 +0100

    More examples

---
+++ examples/Example6_Collision/Example6_Collision.ino
@@ -0,0 +1,254 @@
+// Copyright (c) 2024 Daniel Wallner
+
+// Send with collision detection.
+// kESIPin and kJamPin must be connected to generate collisions.
+
+// DECODE_COLLISION_DETECTION determines if collision detection is done with the CheckingPinWriter
+// or by comparing the sent message with the decoded message from the output pin.
+#define DECODE_COLLISION_DETECTION 1
+
+#define INS_FAST_TIME 0
+
+#include <Inseparates.h>
+#include <DebugUtils.h>
+
+#include <ProtocolUtils.h>
+#include <ProtocolESI.h>
+
+#if !defined(D4) && defined(PD4)
+#define D4 PD4
+#endif
+#if !defined(D5) && defined(PD5)
+#define D5 PD5
+#endif
+
+#ifndef INPUT_PULLDOWN
+// On boards without INPUT_PULLDOWN an external pulldown must be connected.
+#define INPUT_PULLDOWN INPUT
+#endif
+
+const uint16_t kESIPin = D4;
+const uint16_t kJamPin = D5;
+
+#define ACTIVE HIGH
+#define MIN_SPACE_AFTER_SEND_MICROS 20000
+#define JAM_LENGTH_MICROS 50000
+#define STEP_SLEEP_MICROS 100
+#define SEND_WAIT_MICROS 50000
+
+using namespace inseparates;
+
+DebugPrinter printer;
+Timekeeper timekeeper;
+Scheduler scheduler;
+
+void InsError(uint32_t error)
+{
+  char errorMsg[5];
+  strncpy(errorMsg, (const char*)&error, 4);
+  errorMsg[4] = 0;
+  Serial.print("ERROR: ");
+  Serial.println(errorMsg);
+  Serial.flush();
+  for(;;) yield();
+}
+
+class MainTask  : public SteppedTask, public RxESI::Delegate, public CheckingPinWriter::Delegate, public Scheduler::Delegate
+{
+#if DECODE_COLLISION_DETECTION
+  OpenDrainPinWriter _pinWriter;
+#else
+  CheckingPinWriter _pinWriter;
+#endif
+  TxESI _txESI;
+  RxESI _esiDecoder;
+  OpenDrainPinWriter _jamWriter;
+  TxJam _jammer;
+  Timekeeper _jamTimekeeper;
+  Timekeeper16 _resendTimekeeper;
+  Timekeeper16 _spaceTimekeeper;
+  uint32_t _nextJamMicros;
+
+  uint8_t _toggle = 1; // Must be 1 to trigger start as _encodedMessage is used as a flag.
+  uint8_t _address = 0;
+  uint8_t _command = 0;
+  uint32_t _encodedMessage = 0;
+  uint32_t _receivedMessage = 0;
+
+public:
+  MainTask() :
+#if DECODE_COLLISION_DETECTION
+    _pinWriter(kESIPin, ACTIVE, INPUT_PULLDOWN),
+#else
+    _pinWriter(kESIPin, 10, this, ACTIVE, INPUT_PULLDOWN),
+#endif
+    _txESI(&_pinWriter, ACTIVE),
+    _esiDecoder(ACTIVE, this),
+    _jamWriter(kJamPin, ACTIVE),
+    _jammer(&_jamWriter, ACTIVE, JAM_LENGTH_MICROS)
+  {
+  }
+
+  void begin()
+  {
+#if !DECODE_COLLISION_DETECTION
+    scheduler.add(&_pinWriter);
+#endif
+    scheduler.add(&_esiDecoder, kESIPin);
+    scheduler.add(this);
+  }
+
+  void RxESIDelegate_data(uint32_t data) override
+  {
+    _receivedMessage = data;
+    printer.print("ESI data: ");
+    printer.println(String(data, HEX));
+  }
+
+  void CheckingPinWriterDelegate_error(uint8_t pin) override
+  {
+    if (scheduler.active(&_txESI))
+    {
+      // Prevent triggering of done.
+      scheduler.remove(&_txESI);
+      printer.println("Collision in message");
+    }
+    else
+    {
+      printer.printf("Collision in space after %ld us\n", (long)_spaceTimekeeper.microsSinceReset());
+    }
+  }
+
+  void SchedulerDelegate_done(SteppedTask *task) override
+  {
+    if (task == &_txESI)
+    {
+      // Send done.
+      // This starts a counter to disable collision detection later.
+      // Doing it here already is not safe.
+      _spaceTimekeeper.reset();
+      return;
+    }
+
+    if (task == &_jammer)
+    {
+      // Jam done.
+      // Schedule next.
+      _nextJamMicros = random(5, 500) * 1000L;
+      _jamTimekeeper.reset();
+      return;
+    }
+  }
+
+  uint16_t SteppedTask_step() override
+  {
+    if (!scheduler.active(&_jammer) && _jamTimekeeper.microsSinceReset() > _nextJamMicros)
+    {
+      printer.println("Jam");
+      scheduler.add(&_jammer, this);
+    }
+
+    if (digitalRead(kESIPin) == ACTIVE)
+    {
+      _resendTimekeeper.reset();
+    }
+
+    if (scheduler.active(&_txESI))
+    {
+      return STEP_SLEEP_MICROS;
+    }
+
+#if DECODE_COLLISION_DETECTION
+    while (_encodedMessage)
+#else
+    if (_pinWriter.enabled())
+#endif
+    {
+      if (_spaceTimekeeper.microsSinceReset() < MIN_SPACE_AFTER_SEND_MICROS)
+      {
+        return STEP_SLEEP_MICROS;
+      }
+#if DECODE_COLLISION_DETECTION
+      if (_receivedMessage != _encodedMessage)
+      {
+        printer.println("Collision");
+        // Resend same data.
+        break;
+      }
+      _encodedMessage = 0;
+#else
+      // Pause collision detection.
+      _pinWriter.disable();
+
+      if (_receivedMessage != _encodedMessage)
+      {
+        printer.println("ERROR: Data corruption!");
+        printer.flush();
+        for(;;) yield();
+      }
+#endif
+
+      // Update data.
+      _toggle ^= 1;
+      ++_command;
+      if (_command & 0x80)
+      {
+        _command = 0;
+        ++_address;
+      }
+    }
+
+    if (_resendTimekeeper.microsSinceReset() > SEND_WAIT_MICROS)
+    {
+      _resendTimekeeper.reset();
+      send();
+    }
+
+    return STEP_SLEEP_MICROS;
+  }
+
+  void send()
+  {
+      printer.println("Send");
+      // Calling flush() is not really advisable normally as this will block.
+      // Used here to force the order of messages.
+      printer.flush();
+#if DECODE_COLLISION_DETECTION
+      _receivedMessage = 0;
+#else
+      // Enable collision detection.
+      _pinWriter.enable();
+#endif
+      _encodedMessage = TxESI::encodeRC5(0, _toggle, _address, _command);
+      // Set sleepUntilRepeat to false here.
+      // Otherwise the transmitter will sleep and not finish until long after MIN_SPACE_AFTER_SEND_MILLIS
+      // which would mean that we would check for collision for too long.
+      _txESI.prepare(_encodedMessage, false);
+      scheduler.add(&_txESI, this);
+  }
+};
+
+MainTask mainTask;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial)
+    delay(50);
+
+  Serial.print("ESI pin: ");
+  Serial.println(kESIPin);
+  Serial.print("Jam pin: ");
+  Serial.println(kJamPin);
+  Serial.flush();
+
+  scheduler.begin();
+  scheduler.add(&printer);
+  mainTask.begin();
+}
+
+void loop()
+{
+  scheduler.poll();
+}
---
+++ examples/Example7_CodeTester/Example7_CodeTester.ino
@@ -0,0 +1,149 @@
+// Copyright (c) 2024 Daniel Wallner
+
+// Sends messages when space bar is presssed.
+// Modify address and command with W A S D or 8 4 5 6 keys.
+// Works best with a standalone serial monitor where you don't have to press enter to trigger send.
+
+// Many devices support additional commands to those available on the remote.
+// By setting the 
+
+#define INS_FAST_TIME 1
+
+#define HW_PWM 0 // Will use timer 2 on AVR.
+#define SW_PWM 0 // Define HW_PWM or SW_PWM to modulate the IR output.
+
+#include <Inseparates.h>
+#include <DebugUtils.h>
+
+#include <ProtocolUtils.h>
+#include <ProtocolRC5.h>
+#include <ProtocolNEC.h>
+
+#if !defined(D3) && defined(PD3)
+#define D3 PD3
+#endif
+
+const uint16_t kIRSendPin = D3;
+
+using namespace inseparates;
+
+DebugPrinter printer;
+Scheduler scheduler;
+
+#if HW_PWM
+PWMPinWriter irPinWriter(kIRSendPin, LOW);
+#elif SW_PWM
+SoftPWMPinWriter irPinWriter(kIRSendPin, LOW);
+#else
+PushPullPinWriter irPinWriter(kIRSendPin);
+#endif
+
+void InsError(uint32_t error)
+{
+  char errorMsg[5];
+  strncpy(errorMsg, (const char*)&error, 4);
+  errorMsg[4] = 0;
+  Serial.print("ERROR: ");
+  Serial.println(errorMsg);
+  Serial.flush();
+  for(;;) yield();
+}
+
+class MainTask  : public SteppedTask
+{
+  TxRC5 _txRC5;
+  Timekeeper _time;
+
+  uint8_t address = 0;
+  uint8_t command = 0;
+  uint8_t toggle = 0;
+
+public:
+  MainTask() :
+    _txRC5(&irPinWriter, LOW)
+  {
+  }
+
+  void begin()
+  {
+    scheduler.add(this);
+  }
+
+  uint16_t SteppedTask_step() override
+  {
+    if (Serial.available())
+    {
+      int c = Serial.read();
+      switch (c)
+      {
+      case 'A':
+      case 'a':
+      case '4':
+        --address;
+        break;
+
+      case 'D':
+      case 'd':
+      case '6':
+        ++address;
+        break;
+
+      case 'W':
+      case 'w':
+      case '8':
+        ++command;
+        break;
+
+      case 'S':
+      case 's':
+      case '5':
+      case '2':
+        --command;
+        break;
+
+      case ' ':
+        {
+          printer.println("Send");
+          toggle ^= 1;
+          uint16_t encodedMessage = _txRC5.encodeRC5(toggle, address, command);
+          _txRC5.prepare(encodedMessage);
+          scheduler.add(&_txRC5);
+          return 10000;
+        }
+      default:
+        return 10000;
+      }
+      printer.printf("A:%hhd C%hhd\n", address, command);
+     }
+    return 10000;
+  }
+};
+
+MainTask mainTask;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial)
+    delay(50);
+
+  Serial.print("IR output pin: ");
+  Serial.println(kIRSendPin);
+  Serial.flush();
+
+  scheduler.begin();
+  scheduler.add(&printer);
+  mainTask.begin();
+
+#if HW_PWM || SW_PWM
+  irPinWriter.prepare(36000, 30);
+#endif
+}
+
+void loop()
+{
+  uint16_t now = fastMicros();
+
+  scheduler.poll();
+}
